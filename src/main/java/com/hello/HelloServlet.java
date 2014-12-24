69bb1a5570d7efa1086450f879a26e1a27aeb140
rajanpatil <rajan.rb@gmail.com>
2014-12-24 17:43:42 +0530

    initial commit for GradleHelloWebApp

---
+++ src/main/java/com/hello/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello from GradleHelloWebApp");
+        writer.flush();
+        writer.close();
+    }
+}
